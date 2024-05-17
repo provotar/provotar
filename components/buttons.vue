24e523df515198409320dfb7e0b4f8776d773134
Adimchi <adimchisylvester2@gmail.com>
2024-05-17 17:13:36 +0100

    Limit number of polls to three and upgraade button

--- components/buttons.vue
+++ components/buttons.vue
@@ -1,13 +1,14 @@
 <script setup>
 defineProps({
     btn_class: String,
+    btn_type: String,
     loading: String,
     isDisabled: Boolean,
 });
 </script>
 
 <template>
-    <button :class="btn_class" @click="$emit('btn_click')" :disabled="isDisabled" :type="type">
+    <button :class="btn_class" @click="$emit('btn_click')" :disabled="isDisabled" :type="btn_type">
         <slot name="icon"></slot>
         <slot />
     </button>
