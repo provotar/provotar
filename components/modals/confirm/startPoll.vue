49cf6f672ce5fa3aa69639be27b43c23727ffec7
Adimchi <adimchisylvester2@gmail.com>
2024-06-27 22:35:50 +0100

    Update to poll setup

--- components/modals/confirm/startPoll.vue
+++ components/modals/confirm/startPoll.vue
@@ -1,20 +1,17 @@
 <script setup>
 import { usePollStore } from '~/store/polls';
 const usePoll = usePollStore();
-
-defineEmits(['closeModal', 'confirmEndPoll'])
-
-
-const endingPoll = ref(null);
-watch(() => usePoll.loadingEndPolls, (newValue, oldValue) => {
-    endingPoll.value = newValue;
+defineEmits(['closeModal', 'confirmStartPoll'])
+const startingPoll = ref(null);
+watch(() => usePoll.loadingStartPolls, (newValue, oldValue) => {
+    startingPoll.value = newValue;
 })
 </script>
 
 <template>
     <ModalsCore modal_class="confirm-modal">
         <template #modal_title>
-            <p class="modal_title">You are about to end this poll</p>
+            <p class="modal_title">You want to start this poll?</p>
         </template>
         <template #modal_icon>
             <PhosphorIconX :size="24" weight="bold" @click="$emit('closeModal')" />
@@ -22,16 +19,17 @@ watch(() => usePoll.loadingEndPolls, (newValue, oldValue) => {
 
         <template #modal_content>
             <div class="content_wrapper flex-col">
-                <p class="confirm_msg">By clicking End poll, invitees will no longer be able to vote. You can resume a
-                    poll after it has ended. Do you want to proceed?</p>
+                <p class="confirm_msg">By clicking Start poll, invitees will be able to start voting. Do you want to
+                    proceed?</p>
                 <div class="modal_ctas flex-row">
                     <p class="back-button" @click="$emit('closeModal')">Cancel</p>
 
 
-                    <Buttons v-if="endingPoll" btn_class="sml_btn pry_purple_loading">
-                        Ending poll...
+                    <Buttons v-if="startingPoll" btn_class="sml_btn pry_purple_loading">
+                        Starting poll...
                     </Buttons>
-                    <Buttons v-else btn_class="sml_btn pry_purple" @click="$emit('confirmEndPoll')">End Poll</Buttons>
+                    <Buttons v-else btn_class="sml_btn pry_purple" @click="$emit('confirmStartPoll')">Start Poll
+                    </Buttons>
 
                 </div>
             </div>
